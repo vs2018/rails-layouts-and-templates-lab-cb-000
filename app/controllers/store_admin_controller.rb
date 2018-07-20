92004851277d748674e7b9328312420235250309
Vishal Sharma <vsh1819@gmail.com>
2018-07-20 10:24:50 +0000

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,18 @@
+class StoreAdminControllerController < ApplicationController
+  
+  layout "admin"
+  
+  def home
+  end
+  
+  def orders
+    render :layout => "order_administration"
+
+  end
+  
+  def invoice
+        render :layout => false
+
+  end
+  
+end
